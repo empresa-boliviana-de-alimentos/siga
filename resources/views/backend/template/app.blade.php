5beb45eca41eb22806878dcc2d688f89cd09b1a5
David Torrez Salinas <ltorrezs2008@gmail.com>
2019-08-23 00:07:26 -0400

    trabajando en estilos

--- resources/views/backend/template/app.blade.php
+++ resources/views/backend/template/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
     @include('backend.template.partials.htmlheader')
     <body>
         @include('backend.template.partials.mainheader')
